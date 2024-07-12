37e751fd25344eb5530789b31039d350ab73b33f
udhaybegyall <udaybegyall12345@gmail.com>
2024-07-12 14:18:50 +0530

    add (auth) layout with logo

--- task-manager-frontend/app/dashboard/layout.tsx
+++ task-manager-frontend/app/dashboard/layout.tsx
@@ -7,7 +7,7 @@ export default function DashboardLayout({
     children: React.ReactNode;
 }) {
     return (
-        <div>
+        <>
             <nav className='flex items-center justify-between fixed top-1 left-1 right-1 bg-background h-16 px-4 sm:px-6 lg:px-8 z-50'>
                 <div className='text-2xl'>Doit.</div>
                 <form
@@ -22,6 +22,6 @@ export default function DashboardLayout({
                 </form>
             </nav>
             {children}
-        </div>
+        </>
     );
 }
