431814341d3af53258848c53e4acbbfe6102e16c
udhaybegyall <udaybegyall12345@gmail.com>
2024-07-19 16:29:06 +0530

    Style: updated dashbord UI

--- task-manager-frontend/app/dashboard/layout.tsx
+++ task-manager-frontend/app/dashboard/layout.tsx
@@ -1,4 +1,5 @@
 import { signOut } from '@/auth';
+import Sidebar from '@/components/sidebar';
 import { Button } from '@/components/ui/button';
 
 export default function DashboardLayout({
@@ -7,21 +8,29 @@ export default function DashboardLayout({
     children: React.ReactNode;
 }) {
     return (
-        <>
-            <nav className='flex items-center justify-between fixed top-1 left-1 right-1 bg-background h-16 px-4 sm:px-6 lg:px-8 z-50'>
-                <div className='text-2xl'>Doit.</div>
-                <form
-                    action={async () => {
-                        'use server';
-                        await signOut();
-                    }}
-                >
-                    <Button type='submit' variant='outline'>
-                        Sign out
-                    </Button>
-                </form>
-            </nav>
-            {children}
-        </>
+        // <>
+        //     <nav className='flex items-center justify-between fixed top-1 left-1 right-1 bg-background h-16 px-4 sm:px-6 lg:px-8 z-50'>
+        //         <div className='text-2xl'>Doit.</div>
+        //         <form
+        //             action={async () => {
+        //                 'use server';
+        //                 await signOut();
+        //             }}
+        //         >
+        //             <Button type='submit' variant='outline'>
+        //                 Sign out
+        //             </Button>
+        //         </form>
+        //     </nav>
+        //     {children}
+        // </>
+        <div className='flex h-screen bg-background'>
+            <Sidebar />
+            <div className='flex-1 flex flex-col overflow-hidden md:ml-20'>
+                <main className='flex-1 overflow-x-hidden overflow-y-auto'>
+                    {children}
+                </main>
+            </div>
+        </div>
     );
 }
--- task-manager-frontend/app/dashboard/page.tsx
+++ task-manager-frontend/app/dashboard/page.tsx
@@ -3,19 +3,27 @@ import { Separator } from '@/components/ui/separator';
 import AddTaskButton from '@/components/task/add-task-button';
 import TaskList from '@/components/task/task-list';
 import TaskFilter from '@/components/task/task-filter';
+import TaskSearch from '@/components/task/task-search';
+import ActiveRemindersPopover from '@/components/task/active-reminders-popover';
 
 async function DashboardPage() {
     return (
-        <div className='mx-auto p-4 max-w-2xl min-h-screen flex flex-col justify-center '>
-            <div className='sticky bg-background top-16 z-50'>
-                <div className='flex justify-between items-center mb-6'>
-                    <h1 className='text-2xl font-bold'>Your Tasks</h1>
-                    <div className='space-x-2 flex w-[30%] items-center'>
-                        <AddTaskButton />
-                        <TaskFilter />
+        <div className='p-4 mx-auto'>
+            <div className='sticky top-0 bg-background z-40'>
+                <div className='pt-4'>
+                    <div className='flex justify-end mb-6'>
+                        <ActiveRemindersPopover />
+                    </div>
+                    <Separator className='mb-6' />
+                    <div className='flex flex-col md:flex-row justify-between mb-4 md:mb-6'>
+                        <TaskSearch />
+                        <div className='flex flex-reverse md:flex-row space-x-2 md:space-x-2 w-full md:w-[200px] items-center'>
+                            <TaskFilter />
+                            <AddTaskButton />
+                        </div>
                     </div>
                 </div>
-                <Separator className='mb-6' />
+                <div className='h-4 bg-background'></div> {/* spacer */}
             </div>
             <TaskList />
         </div>
